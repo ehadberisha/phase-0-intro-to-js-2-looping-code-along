24313d6fab0396cbbc066913251ae52567c18052
Ehad <ehadberisha@gmail.com>
2023-08-14 17:48:56 -0400

    finished stuff

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  const thankYouMessages = [];
+  let i = 0;
+  while (i < names.length) {
+    thankYouMessages.push(
+      `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    );
+    i++;
+  }
+  return thankYouMessages;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number--);
+  }
+}
